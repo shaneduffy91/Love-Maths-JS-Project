37cda333ecee7add636a6b24c981197ca06b7b52
shaneduffy91 <shaneduffy38@yahoo.ie>
2023-07-13 13:11:34 +0000

    Add favicon, and final tidy ups for the user

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -15,7 +15,16 @@ document.addEventListener("DOMContentLoaded", function() {
        }) 
    }
 
+   document.getElementById("answer-box").addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        checkAnswer();
+    }
+   })
+
     runGame("addition");
+    runGame("multiply");
+    runGame("subtract");
+    runGame("division");
 })
 
 /** 
@@ -24,12 +33,22 @@ document.addEventListener("DOMContentLoaded", function() {
 */
 function runGame(gameType) {
 
+    document.getElementById("answer-box").value = " ";
+    document.getElementById("answer-box").focus();
+
+
     // creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
 
     if (gameType === "addition") {
-       displayAdditionQuestion(num1, num2); 
+       displayAdditionQuestion(num1, num2);
+    } else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2); 
+    } else if (gameType === "subtract") {
+        displaySubtractionQuestion(num1, num2);
+    } else if (gameType === "division") {
+        displayDivisionQuestion(num1, num2);
     } else {
         alert (`Unknown game type: ${gameType}`);
         throw  `Unknown game type: ${gameType}, Aborting!`;
@@ -70,10 +89,17 @@ function calculateCorrectAnswer() {
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
+    } else if (operator === "x") {
+        return [operand1 * operand2, "multiply"];
+    } else if (operator === "-") {
+        return [operand1 - operand2, "subtract"];
+    } else if (operator === "/") {
+        return [(operand1 / operand2), "division"];                                   
     } else {
         alert(`unimplemented operator ${operator}`);
         throw `unimplemented operator ${operator}.Aborting!`;
     }
+
 }
 
 
@@ -101,10 +127,41 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = "+";
 }
 
-function displaySubtractionQuestion() {
+function displaySubtractionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
+}
+
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+     document.getElementById('operand1').textContent = operand1 > operand2 ? Math.round(random / operand1)*operand1 : Math.round(random / operand2)*operand2;
+     document.getElementById('operand2').textContent = operand1 > operand2 ? Math.round(random / operand2)*operand2 : Math.round(random / operand1)*operand1;
+     document.getElementById('operator').textContent = "/";
 
 }
 
-function displayMultiplyQuestion() {
+    //let divisor1 = document.getElementById('operand1').textContent;
+    //let divisor2 = document.getElementById('operand2').textContent;
+    //let even1 = divisor1           //%2==0;
+    //let even2 = divisor2            //%2==0;
 
-}
+    
+
+    //if (divisor1 > divisor2  & divisor1 % divisor2 ==0) {
+     //   return divisor1;
+    //} else if (divisor1 > divisor2 & divisor2 % divisor1 ==0) {
+     //   return divisor2;
+    //}
+
+
+
+ //const specialRandom = (num = 1, limit = 1000000) =>
+ // rounding it off to be divisible by num
+ //const res = Math.round( random / num ) * num;
+ //return res;
